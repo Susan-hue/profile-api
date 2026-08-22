3a7877caf6a628da35d4d41cd22b4e07e0cc99b3
Susan Amechi <amechisusanogechi@gmail.com>
2026-08-22 22:20:35 +0100

    Add Render deployment config

--- profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProfileMeView, AvatarUploadView
-
-urlpatterns = [
-    path('profile/me/', ProfileMeView.as_view(), name='profile-me'),
-    path('profile/avatar/', AvatarUploadView.as_view(), name='profile-avatar'),
-]
